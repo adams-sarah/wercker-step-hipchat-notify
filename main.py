fb3b2b679744f6be9fae0c64d5fde4ae46ea3075
Sarah Adams <sadams.codes@gmail.com>
2015-04-27 11:13:43 -0700

    changes for hipchat v2 api

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-import hipchat
+import requests
+import json
 
 if len(sys.argv) < 8:
     print 'There should be 8 arguments'
@@ -16,13 +17,15 @@ notify = '0'
 if str(sys.argv[6]) == 'true':
     notify = '1'
 
-hipster = hipchat.HipChat(token=token)
-
-hipster.method('rooms/message', method='POST', parameters={
-    'room_id': room_id,
-    'from': from_name,
-    'message': message,
+query = {'auth_token': token}
+headers = {'Content-type': 'application/json'}
+body = {
     'color': color,
+    'message': message,
     'notify': notify,
-    'message_format': message_format
-})
+    'message_format': message_format,
+}
+
+resp = requests.post('https://api.hipchat.com/v2/room/'+room_id+'/notification', params=query, headers=headers, data=json.dumps(body))
+
+resp.raise_for_status()
